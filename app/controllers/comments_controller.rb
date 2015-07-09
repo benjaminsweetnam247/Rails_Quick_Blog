5d0baa58fcd0a455ea14fd699a4b21acdd495539
benjamin sweetnam <ben_s247@optusnet.com.au>
2015-07-09 23:52:09 +1000

    Fix Comment Creation
    removes the s from @posts in comments_controller.rb line 3
    
    Signed-off-by: benjamin sweetnam <ben_s247@optusnet.com.au>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 	def create
-		@posts = Post.find(params[:post_id])
+		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create!(params[:comment])
 		redirect_to @post
 	end
